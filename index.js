48c2d3aa123e3a108990c85e390a41f87aa5b3d3
Harry White <harry.white424@gmail.com>
2018-06-08 11:04:10 +1000

    switched to get request, added function to transform data into 3d-force-graph format

--- index.js
+++ index.js
@@ -1,24 +1,82 @@
 var default_uuid = "6b5b5d6a-158c-11e7-803e-0242ac110017"
 
-post_data = {
-  'query': '{ { metadata (uuid: \"' + default_uuid + '\") { edges { node { name } } } }',
-}
+graphql_query = '{ metadata (uuid: \"' + default_uuid + '\") { edges { node { uuid name } } } }',
+base_url = 'https://registry.aristotlemetadata.com/api/graphql/api?raw=true'
 
 request_options = {
-  'method': 'POST',
+  'method': 'GET',
   'headers': {
     'Accept': 'application/json',
-    'Content-Type': 'application/json',
   },
   'mode': 'cors',
-  'referrer': 'no-referrer',
-  'body': JSON.stringify(post_data)
 }
 
+url = base_url + '&query=' + graphql_query
 
-fetch('https://registry.aristotlemetadata.com/api/graphql/', request_options).then(
+display_data = {}
+
+function reset_data() {
+  display_data = {
+    'nodes': [],
+    'links': []
+  }
+}
+
+function dfs(data, superitem) {
+  // Depth first search on returned graphql data
+  // to build 3d-force-graph data
+  // assumes structure is a tree
+  
+  console.log(data)
+  keys = Object.keys(data)
+  //console.log(keys)
+  
+  if ('uuid' in data) {
+    superitem = data['uuid']
+  }
+
+  for (i=0; i < keys.length; i++) {
+    key = keys[i]
+
+    if (key == 'edges') {
+      for (j=0; j < data['edges'].length; j++) {
+        edge = data['edges'][j]
+        dfs(edge, superitem)
+      }
+    } else if (key == 'node') {
+      //console.log('adding a node')
+      display_data['nodes'].push({
+        'id': data['node']['uuid'],
+        'name': data['node']['name'],
+        'val': 1
+      })
+      
+      if (superitem != null) {
+        display_data['links'].push({
+          'source': superitem,
+          'target': data['node']['uuid']
+        })
+      }
+
+      dfs(data['node'], superitem)
+    } else {
+      item = data[key]
+      console.log(typeof item)
+      if (typeof item == 'object') {
+        dfs(item, superitem)
+      }
+    }
+  }
+
+}
+
+fetch(url, request_options).then(
   function(response) {
     console.log(response.status)
-    console.log(status)
+    response.json().then(
+      function(data) {
+        reset_data()
+        dfs(data['data'], null)
+    })
   }
 );

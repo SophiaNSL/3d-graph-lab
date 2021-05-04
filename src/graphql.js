40862c12eef6636b6ea30289c2e2ebc32e3c97df
Francisco Gallardo <fragalcer@gmail.com>
2021-05-04 15:34:15 +1000

    Provide API Token to fetch private metadata.

--- src/graphql.js
+++ src/graphql.js
@@ -1,105 +1,93 @@
 import { jsonToGraphQLQuery } from 'json-to-graphql-query'
+const axios = require('axios')
 
-function build_graphql_query(uuid) {
-
-  var graphql_query = {
-    query: {
-      datasetSpecifications: {
-        __args: {
-          uuid: uuid
-        },
-        edges: {
-          node: {
-            name: true,
-            uuid: true,
-            dssdeinclusionSet: {
-              dataElement: {
-                uuid: true,
-                name: true,
-                dataElementConcept: {
-                  uuid: true,
-                  name: true,
-                  objectClass: {
-                    uuid: true,
-                    name: true
-                  },
-                  property: {
-                    uuid: true,
-                    name: true
-                  }
-                },
-                valueDomain: {
-                  uuid: true,
-                  name: true
+function buildGraphqlQuery(uuid) {
+    return jsonToGraphQLQuery(
+        {
+            query: {
+                datasetSpecifications: {
+                    __args: {
+                        uuid: uuid
+                    },
+                    edges: {
+                        node: {
+                            name: true,
+                            uuid: true,
+                            dssdeinclusionSet: {
+                                dataElement: {
+                                    uuid: true,
+                                    name: true,
+                                    dataElementConcept: {
+                                        uuid: true,
+                                        name: true,
+                                        objectClass: {
+                                            uuid: true,
+                                            name: true
+                                        },
+                                        property: {
+                                            uuid: true,
+                                            name: true
+                                        }
+                                    },
+                                    valueDomain: {
+                                        uuid: true,
+                                        name: true
+                                    }
+                                }
+                            }
+                        }
+                    }
                 }
-              }
             }
-          }
         }
-      }
-    }
-  }
-
-  return jsonToGraphQLQuery(graphql_query)
-}
-
-function graphql_request(baseurl, query, callback, error_callback) {
-  
-  // Set request params
-  const request_options = {
-    'method': 'GET',
-    'headers': {
-      'Accept': 'application/json',
-    },
-    'mode': 'cors',
-  }
-
-  // Set url
-  var api_url = 'https://' + baseurl + '/api/graphql/api?raw=true'
-  var url = api_url + '&query=' + query
-
-  fetch(url, request_options)
-    .then(
-      function(response) {
-        if (response.status != 200) {
-          error_callback(response.status)
-        } else {
-          response.json()
-            .then(callback)
-            .catch(error_callback)
-        }
-      }
     )
-    .catch(error_callback)
-
 }
 
-export function gql_search(baseurl, search_text, callback, error_callback) {
-
-  var search_query = jsonToGraphQLQuery({
-    query: {
-      datasetSpecifications: {
-        __args: {
-          name_Icontains: search_text,
-          first: 5
-        },
-        edges: {
-          node: {
-            name: true,
-            uuid: true
-          }
-        }
-      }
+function graphqlRequest(baseurl, query, callback, errorCallback, apiToken) {
+    let headers = {
+        'Accept': 'application/json',
+        'Content-Type': 'application/graphql',
     }
-  })
-  graphql_request(baseurl, search_query, callback, error_callback)
 
+    if (apiToken) {
+        headers.Authorization = `Token ${apiToken}`
+    }
+
+    axios({
+        method: 'post',
+        headers: headers,
+        url: `https://${baseurl}/api/graphql/json`,
+        data: query,
+    }).then((response) => {
+        if (response.status !== 200) {
+            errorCallback(response.status)
+        } else {
+            callback(response.data)
+        }
+    }).catch(errorCallback)
 }
 
-export function gql_request(baseurl, uuid, callback, error_callback) {
-
-  var gql_text_query = build_graphql_query(uuid)
-  graphql_request(baseurl, gql_text_query, callback, error_callback)
-
+export function gqlSearch(baseurl, searchText, callback, errorCallback, apiToken) {
+    let searchQuery = jsonToGraphQLQuery({
+        query: {
+            datasetSpecifications: {
+                __args: {
+                    name_Icontains: searchText,
+                    first: 5
+                },
+                edges: {
+                    node: {
+                        name: true,
+                        uuid: true
+                    }
+                }
+            }
+        }
+    })
+    graphqlRequest(baseurl, searchQuery, callback, errorCallback, apiToken)
 }
 
+export function gqlRequest(baseurl, uuid, callback, error_callback, apiToken) {
+    let gql_text_query = buildGraphqlQuery(uuid)
+    graphqlRequest(baseurl, gql_text_query, callback, error_callback, apiToken)
+}
--- src/index.js
+++ src/index.js
@@ -2,242 +2,251 @@ import './style.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 import Vue from 'vue'
-import { gql_request, gql_search } from './graphql.js'
+import {gqlRequest, gqlSearch} from './graphql.js'
 
-var vm = new Vue({
-  el: '#vue',
-  data: {
-    display_data: {},
-    seen_uuids: [],
-    uuid_input: '66eeaffc-158c-11e7-803e-0242ac110017',
-    baseurl: 'registry.aristotlemetadata.com',
-    search_text: '',
-    display_name: '',
-    loading: true,
-    search_loading: false,
-    search_results: {},
-    search_display: false,
-    display_info: {},
-    error_message: '',
-    colormap: {
-      datasetSpecification: '#e31a1c',
-      dataElement: '#1f78b4',
-      dataElementConcept: '#b2df8a',
-      objectClass: '#33a02c',
-      valueDomain: '#a6cee3',
-      property: '#fb9a99'
-    }
-  },
-  mounted: function() {
-    this.initGraph()
-  },
-  computed: {
-    prettymap: function() {
-      var pmap = {}
-      for (var key in this.colormap) {
-        var newkey = this.uncamel(key)
-        pmap[newkey] = this.colormap[key]
-      }
-      return pmap
-    }
-  },
-  methods: {
-    request_uuid: function(uuid) {
-      // Request a dss by uuid and display it
-      this.loading = true
-      this.uuid_input = uuid
-      this.request()
-    },
-    request: function() {
-      // Request the current uuid and display it if successfull
-      this.loading = true
-      var currentvue = this
-      gql_request(
-        this.baseurl,
-        currentvue.uuid_input, 
-        function(data) {
-          // console.log(data)
-          if ('datasetSpecifications' in data['data']) {
-            currentvue.reset_data()
-            currentvue.dfs(data['data'], null, 'datasetSpecification')
-            currentvue.display_name = data['data']['datasetSpecifications']['edges'][0]['node']['name']
-          } else {
-            currentvue.error_message = "Not a valid Dataset UUID"
-          }
-          currentvue.loading = false
+new Vue({
+    el: '#vue',
+    data: {
+        displayData: {},
+        seenUuids: [],
+        uuidInput: '5ce5c692-ac6b-11eb-93e6-0a58a9feac02',
+        baseurl: 'staging.sydney.aristotlecloud.io',
+        // uuidInput: '66eeaffc-158c-11e7-803e-0242ac110017',
+        // baseurl: 'registry.aristotlemetadata.com',
+        apiToken: '8c8ee720107fe4e6841439584b7661de2d3a7f60',
+        apiTokenInputType: 'password',
+        apiTokenButtonText: 'Show Token',
+        searchText: '',
+        displayName: '',
+        loading: true,
+        searchLoading: false,
+        searchResults: {},
+        searchDisplay: false,
+        displayInfo: {},
+        errorMessage: '',
+        colormap: {
+            datasetSpecification: '#e31a1c',
+            dataElement: '#1f78b4',
+            dataElementConcept: '#b2df8a',
+            objectClass: '#33a02c',
+            valueDomain: '#a6cee3',
+            property: '#fb9a99',
         },
-        function(error) {
-          currentvue.error_message = "Request could not be completed"
-          currentvue.loading = false
-        }
-      )
     },
-    search: function() {
-      // Perform a search request and display results
-      this.search_results = {}
-      this.search_loading = true
-      this.search_display = true
-      this.error_message = ''
-      var cvue = this
-      gql_search(
-        this.baseurl,
-        cvue.search_text,
-        function(data) {
-          cvue.search_results = data['data']['datasetSpecifications']['edges']
-          cvue.search_loading = false
-        },
-        function(error) {
-          cvue.search_results = {}
-          cvue.search_loading = false
-          cvue.error_message = "Search could not be completed"
-        }
-      )
+    mounted: function() {
+        this.initGraph()
     },
-    searchHide: function(event) {
-      // Hide the search results on focusout
-      if (event.relatedTarget != null) {
-        if (!event.relatedTarget.classList.contains('list-group-item')) {
-          this.search_display=false
-        }
-      } else {
-        this.search_display=false
-      }
-    },
-    searchKey: function(event) {
-      // Search box on keyup
-      if (event.keyCode == 13) {
-        var button = document.getElementById('searchButton')
-        button.focus()
-        button.click()
-      }
-    },
-    dfs: function(data, superitem, type) {
-      // Depth first search on returned graphql data
-      // to build 3d-force-graph data
-      // assumes structure is a tree
-      
-      // console.log(data)
-      var keys = Object.keys(data)
-      //console.log(keys)
-      
-      if ('uuid' in data) {
-        superitem = data['uuid']
-      }
-
-      for (var i=0; i < keys.length; i++) {
-        var key = keys[i]
-
-        if (key == 'edges') {
-          for (var j=0; j < data['edges'].length; j++) {
-            var edge = data['edges'][j]
-            this.dfs(edge, superitem, type)
-          }
-        } else if (key == 'dssdeinclusionSet') {
-          for (var k=0; k < data['dssdeinclusionSet'].length; k++) {
-            var item = data['dssdeinclusionSet'][k]
-            this.dfs(item, superitem, type)
-          }
-        } else if (key == 'node') {
-          this.add_display_data(data['node'], superitem, type)
-          this.dfs(data['node'], superitem, type)
-        } else {
-          var item = data[key]
-          // console.log(typeof item)
-          if (typeof item == 'object') {
-            if (item != null) {
-              this.add_display_data(item, superitem, key)
-              this.dfs(item, superitem, type)
+    computed: {
+        prettyMap: function() {
+            let pMap = {}
+            for (let key in this.colormap) {
+                let newKey = this.unCamel(key)
+                pMap[newKey] = this.colormap[key]
             }
-          }
-        }
-      }
+            return pMap
+        },
     },
-    add_display_data: function(node, superitem, type) {
-      // Adds data returned by dfs to the graph
-      
-      if ('uuid' in node && 'name' in node) {
+    methods: {
+        requestUuid: function(uuid) {
+            // Request a dss by uuid and display it.
+            this.loading = true
+            this.uuidInput = uuid
+            this.request()
+        },
+        request: function() {
+            // Request the current uuid and display it if successful.
+            this.loading = true
+            let currentVue = this
+            gqlRequest(
+                this.baseurl,
+                currentVue.uuidInput,
+                function(data) {
+                    if ('datasetSpecifications' in data['data']) {
+                        currentVue.resetData()
+                        currentVue.dfs(data['data'], null, 'datasetSpecification')
+                        currentVue.displayName = data['data']['datasetSpecifications']['edges'][0]['node']['name']
+                    } else {
+                        currentVue.errorMessage = "Not a valid Dataset UUID"
+                    }
+                    currentVue.loading = false
+                },
+                function(error) {
+                    currentVue.errorMessage = "Request could not be completed"
+                    currentVue.loading = false
+                },
+                this.apiToken
+            )
+        },
+        search: function() {
+            // Perform a search request and display results.
+            this.searchResults = {}
+            this.searchLoading = true
+            this.searchDisplay = true
+            this.errorMessage = ''
+            let cVue = this
+            gqlSearch(
+                this.baseurl,
+                cVue.searchText,
+                function(data) {
+                    cVue.searchResults = data['data']['datasetSpecifications']['edges']
+                    cVue.searchLoading = false
+                },
+                function(error) {
+                    cVue.searchResults = {}
+                    cVue.searchLoading = false
+                    cVue.errorMessage = "Search could not be completed"
+                },
+                this.apiToken
+            )
+        },
+        searchHide: function(event) {
+            // Hide the search results on focusout.
+            if (event.relatedTarget != null) {
+                if (!event.relatedTarget.classList.contains('list-group-item')) {
+                    this.searchDisplay=false
+                }
+            } else {
+                this.searchDisplay=false
+            }
+        },
+        searchKey: function(event) {
+            // Search box on keyup.
+            if (event.keyCode === 13) {
+                let button = document.getElementById('searchButton');
+                button.focus()
+                button.click()
+            }
+        },
+        dfs: function(data, superItem, type) {
+            // Depth first search on returned graphql data to build 3d-force-graph data.
+            // Assumes structure is a tree.
 
-        if (!this.seen_uuids.includes(node['uuid'])) {
+            let keys = Object.keys(data)
 
-          var nodeobj = {
-            'id': node['uuid'],
-            'name': node['name'],
-            'val': 1,
-            'type': type
-          }
+            if ('uuid' in data) {
+                superItem = data['uuid']
+            }
 
-          var nodecolor = this.colormap[type]
-          if (nodecolor != undefined) {
-            nodeobj.color = nodecolor
-          }
+            for (let i=0; i < keys.length; i++) {
+                let key = keys[i]
+
+                if (key === 'edges') {
+                    for (let j=0; j < data['edges'].length; j++) {
+                        let edge = data['edges'][j];
+                        this.dfs(edge, superItem, type)
+                    }
+                } else if (key === 'dssdeinclusionSet') {
+                    for (let k=0; k < data['dssdeinclusionSet'].length; k++) {
+                        let item = data['dssdeinclusionSet'][k]
+                        this.dfs(item, superItem, type)
+                    }
+                } else if (key === 'node') {
+                    this.addDisplayData(data['node'], superItem, type)
+                    this.dfs(data['node'], superItem, type)
+                } else {
+                    let item = data[key]
+                    if (typeof item == 'object') {
+                        if (item != null) {
+                            this.addDisplayData(item, superItem, key)
+                            this.dfs(item, superItem, type)
+                        }
+                    }
+                }
+            }
+        },
+        addDisplayData: function(node, superItem, type) {
+            // Adds data returned by dfs to the graph.
+
+            if ('uuid' in node && 'name' in node) {
+
+                if (!this.seenUuids.includes(node['uuid'])) {
+
+                    let nodeObj = {
+                        'id': node['uuid'],
+                        'name': node['name'],
+                        'val': 1,
+                        'type': type
+                    };
+
+                    let nodeColor = this.colormap[type];
+                    if (nodeColor !== undefined) {
+                        nodeObj.color = nodeColor
+                    }
 
 
-          this.display_data['nodes'].push(nodeobj)
-          this.seen_uuids.push(node['uuid'])
-        }
-        
-        if (superitem != null) {
-          this.display_data['links'].push({
-            'source': superitem,
-            'target': node['uuid']
-          })
-        }
+                    this.displayData['nodes'].push(nodeObj)
+                    this.seenUuids.push(node['uuid'])
+                }
 
-        this.aristotleGraph.graphData(this.display_data);
+                if (superItem != null) {
+                    this.displayData['links'].push({
+                        'source': superItem,
+                        'target': node['uuid']
+                    })
+                }
 
-      }
+                this.aristotleGraph.graphData(this.displayData);
+
+            }
+        },
+        resetData: function() {
+            // Reset the data, used on new load.
+            this.displayData = {
+                'nodes': [],
+                'links': []
+            }
+            this.seenUuids = []
+            this.displayInfo = {}
+            this.errorMessage = ''
+            this.searchResults = {}
+            this.searchDisplay = false
+        },
+        initGraph: function() {
+            // Initialise the graph.
+            this.loading = true
+
+            import(/* webpackChunkName: "3d-force-graph" */ '3d-force-graph').then(tdfg => {
+
+                let ForceGraph3D = tdfg.default
+
+                // Setup Graph
+                let graphElem = document.getElementById('3d-graph')
+                this.aristotleGraph = ForceGraph3D()(graphElem)
+                    .nodeAutoColorBy('type')
+                    .width(1280)
+                    .height(720)
+                    .onNodeClick(this.setDisplayInfo)
+
+                this.request()
+            })
+        },
+        setDisplayInfo: function(node) {
+            // Set the display info (runs on node click).
+            let fullUrl = 'https://' + this.baseurl + '/item/'
+
+            let displayType = this.unCamel(node.type)
+
+            this.displayInfo = {
+                Name: node.name,
+                Type: displayType,
+                Link: fullUrl + node.id
+            }
+        },
+        unCamel: function(text) {
+            return text
+                .replace(/([A-Z])/g, ' $1')  // Spaces before caps
+                .replace(/^./, function (str) {
+                    return str.toUpperCase()  // Capitalise first letter
+                })
+        },
+        showToken: function () {
+            if (this.apiTokenInputType === 'text') {
+                this.apiTokenInputType = 'password'
+                this.apiTokenButtonText = 'Show Token'
+            } else if (this.apiTokenInputType === 'password') {
+                this.apiTokenInputType = 'text'
+                this.apiTokenButtonText = 'Hide Token'
+            }
+        },
     },
-    reset_data: function() {
-      // Reset the data, used on new load
-      this.display_data = {
-        'nodes': [],
-        'links': []
-      }
-      this.seen_uuids = []
-      this.display_info = {}
-      this.error_message = ''
-      this.search_results = {}
-      this.search_display = false
-    },
-    initGraph: function() {
-      // Initialise the graph
-      this.loading = true
-
-      import(/* webpackChunkName: "3d-force-graph" */ '3d-force-graph').then(tdfg => {
-
-        var ForceGraph3D = tdfg.default
-
-        // Setup Graph
-        var graph_elem = document.getElementById('3d-graph')
-        this.aristotleGraph = ForceGraph3D()(graph_elem)
-          .nodeAutoColorBy('type')
-          .width(1280)
-          .height(720)
-          .onNodeClick(this.setDisplayInfo)
-
-        this.request()
-      })
-    },
-    setDisplayInfo: function(node) {
-      // Set the display info (runs on node click)
-      var full_url = 'https://' + this.baseurl + '/item/'
-
-      var display_type = this.uncamel(node.type)
-
-      this.display_info = {
-        Name: node.name,
-        Type: display_type,
-        Link: full_url + node.id
-      }
-    },
-    uncamel: function(text) {
-      var display_text = text
-        // Spaces before caps
-        .replace(/([A-Z])/g, ' $1')
-        // Capitalise first letter
-        .replace(/^./, function(str){ return str.toUpperCase(); })
-
-      return display_text
-    }
-  }
-});
+})
